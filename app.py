b054d5da47d042084fe4dbc6ef5165fa8f0ec7f5
Seungsoo <seungsoo0701@gmail.com>
2022-01-10 19:57:48 +0900

    pip install moduels
    pip install -r requests.txt

--- app.py
+++ app.py
@@ -34,30 +34,6 @@ import hashlib
 
 
 
-```python
-from flask import Flask, render_template, jsonify, request, session, redirect, url_for
-
-app = Flask(__name__)
-
-from pymongo import MongoClient
-
-client = MongoClient('mongodb://3.34.44.93', 27017, username="sparta", password="woowa")
-db = client.dbsparta_plus_week4
-
-# JWT 토큰을 만들 때 필요한 비밀문자열입니다. 아무거나 입력해도 괜찮습니다.
-# 이 문자열은 서버만 알고있기 때문에, 내 서버에서만 토큰을 인코딩(=만들기)/디코딩(=풀기) 할 수 있습니다.
-SECRET_KEY = 'SPARTA'
-
-# JWT 패키지를 사용합니다. (설치해야할 패키지 이름: PyJWT)
-import jwt
-
-# 토큰에 만료시간을 줘야하기 때문에, datetime 모듈도 사용합니다.
-import datetime
-
-# 회원가입 시엔, 비밀번호를 암호화하여 DB에 저장해두는 게 좋습니다.
-# 그렇지 않으면, 개발자(=나)가 회원들의 비밀번호를 볼 수 있으니까요.^^;
-import hashlib
-
 #################################
 ##  HTML을 주는 부분             ##
 #################################
